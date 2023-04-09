b3f1bd88c18d735d5b8b0627bb356c83554b551a
Fernando Labra Caso <fernandolabracaso@gmail.com>
2023-04-09 12:23:18 +0200

    Update model with one more convolution layer

--- lab-1/utils.py
+++ lab-1/utils.py
@@ -7,21 +7,25 @@ class Net(nn.Module):
     def __init__(self, activation):
         super().__init__()
 
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        # Convolutional Layers
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
+        # Max Pooling Layer
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
+        # Fully Connected Layers
+        self.fc1 = nn.Linear(128 * 4 * 4, 512)
+        self.fc2 = nn.Linear(512, 10)
+        # Activation Layer
         self.activation = nn.LeakyReLU(negative_slope=.1) if activation == "LeakyReLu" else nn.Tanh()
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = self.pool(self.activation(self.conv1(x)))
         x = self.pool(self.activation(self.conv2(x)))
+        x = self.pool(self.activation(self.conv3(x)))
         x = torch.flatten(x, 1)  # flatten all dimensions except batch
         x = self.activation(self.fc1(x))
-        x = self.activation(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
 
 
